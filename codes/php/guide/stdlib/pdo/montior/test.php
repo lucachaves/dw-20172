1da5f985eceeb82d41d42157e6f92398d5edfe05
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2018-02-23 08:13:23 -0300

    atualizando o modelo do ping-api-db

--- codes/php/guide/stdlib/pdo/montior/test.php
+++ codes/php/guide/stdlib/pdo/montior/test.php
@@ -11,18 +11,26 @@ $icmp = new Icmp();
 $packet = new Packet();
 $host = new Host();
 
-$lastInsertId = $host->create('www.ifpb.edu.br', '200.129.77.62');
+$hostId = $host->create('www.ifpb.edu.br', '200.129.77.62');
 
-$lastInsertId = $icmp->create(2, 2, $lastInsertId);
+$icmpId = $icmp->create(2, 2, $hostId);
 
-$packet->create(0, 63, 123, $lastInsertId);
-$packet->create(1, 63, 121, $lastInsertId);
+$packet->create(0, 63, 123, $icmpId);
+$packet->create(1, 63, 121, $icmpId);
 
-$lastInsertId = $host->create('www.ifrn.edu.br', '200.137.2.130');
 
-$lastInsertId = $icmp->create(2, 2, $lastInsertId);
+$hostId = $host->readOrCreate('www.ifpb.edu.br', '200.129.77.62');
 
-$packet->create(0, 63, 104, $lastInsertId);
-$packet->create(1, 63, 104, $lastInsertId);
+$icmpId = $icmp->create(2, 2, $hostId);
+
+$packet->create(0, 63, 123, $icmpId);
+$packet->create(1, 63, 121, $icmpId);
+
+$hostId = $host->create('www.ifrn.edu.br', '200.137.2.130');
+
+$icmpId = $icmp->create(2, 2, $hostId);
+
+$packet->create(0, 63, 104, $icmpId);
+$packet->create(1, 63, 104, $icmpId);
 
 print_r($icmp->readJoinAll());
--- codes/php/guide/web/api/ping-api-db/v1/ping.php
+++ codes/php/guide/web/api/ping-api-db/v1/ping.php
@@ -1,69 +1,26 @@
 <?php
+// v1/ping.php?host=8.8.8.8&count=1
+namespace Api;
 
-  require_once __DIR__."/../model/icmp-request-db.php";
+use Util\Ping;
 
-  function extractPingInfo($result) {
-    $json["ip"] = "";
-    $json["packets"] = [];
-    $json["statistics"] = [];
+require_once __DIR__."/../util/ping.php";
 
-    $regex = "/\(([\d\.]+)\)/";
-    preg_match($regex, $result, $matches);
-    $json["ip"] = $matches[1];
+$host = $_GET["host"] ?? null;
+$count = $_GET["count"] ?? 1;
 
-    $regex = "/icmp_seq=(\d+) ttl=(\d+) time=([\d\.]+)/";
-    preg_match_all($regex, $result, $matches);
-    foreach($matches[1] as $key => $sequence){
-      $json["packets"][] = [
-        "seq" => (int) $matches[1][$key],
-        "ttl" => (int) $matches[2][$key],
-        "time" => (float) $matches[3][$key]
-      ];
-    }
+$json = '';
 
-    $regex = "/(\d+) packets transmitted, (\d+) (packets received|received)/";
-    preg_match($regex, $result, $matches);
-    $json["statistics"]["transmitted"] = (int) $matches[1];
-    $json["statistics"]["received"] = (int) $matches[2];
-    $json["statistics"]["losted"] = $matches[1] - $matches[2];
+if($host && $pingInfo){
+  http_response_code(500);
+  $json = ["error" => "Unknown host"];
+} else {
+  $ping = new Ping();
+  $pingInfo = $ping->exec($host, $count);
+  $json = $pingInfo;
+}
 
-    $regex = "/min\/avg\/max\/(stddev|mdev) = ([\d\.]+)\/([\d\.]+)\/([\d\.]+)\/([\d\.]+)/";
-    preg_match($regex, $result, $matches);
-    $json["statistics"]["min"] = (float) $matches[1];
-    $json["statistics"]["avg"] = (float) $matches[2];
-    $json["statistics"]["max"] = (float) $matches[3];
-    $json["statistics"]["stddev"] = (float) $matches[4];
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
 
-    return $json;
-  }
-
-  $host = $_GET["host"] ?? null;
-  $count = $_GET["count"] ?? 1;
-  $jsonError = ["error" => "Unknown host"];
-  $pingInfo = [];
-
-  $command = "ping -c{$count} {$host}";
-  $result = shell_exec($command);
-
-  if($result){
-    $pingInfo = extractPingInfo($result);
-    $pingInfo["host"] = $host;
-  }
-
-  $response = ($host && $pingInfo) ? $pingInfo : $jsonError ;
-
-  if(isset($response["error"])){
-    http_response_code(500);
-  }
-  else {
-    $icmpResquetDb = new IcmpRequestDb("mysql", "ping_api", "localhost", "root", "abc123");
-    $address = $pingInfo["host"];
-    $ip = $pingInfo["ip"];
-    $transmitted = $pingInfo["statistics"]["transmitted"];
-    $received = $pingInfo["statistics"]["received"];
-    $icmpResquetDb->create($address, $ip, $transmitted, $received);
-  }
-
-  header("Content-type: application/json; charset=UTF-8");
-  header("Access-Control-Allow-Origin: *");
-  echo json_encode($response);
+echo json_encode($json);
